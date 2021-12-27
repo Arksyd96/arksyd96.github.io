02fe7788123bdfcf37f6cf1dd3c879abf03b7a6c
Arksyd96 <loupblanc1998@gmail.com>
2021-12-27 14:33:29 +0100

    i think now it works

--- pages/blog/[post].js
+++ pages/blog/[post].js
@@ -11,16 +11,12 @@ import ToggleButton from "../../src/Components/ToggleButton";
 import { promises as fs } from "fs";
 import path from "path";
 
-// const posts = require('../../static-data/posts.json');
-
 const Post = props => {
-    // const [enableParticles, setEnableParticles] = React.useState(true);
-
-    // const post = posts.find(post => post.id === props.title)  
+    const [enableParticles, setEnableParticles] = React.useState(true);
 
     return (
         <div className="App">
-            {/* <Header blog />
+            <Header blog />
             <Layout>
                 <SocialMediaNav />
                 <ParticlesNetwork enableParticles={enableParticles} />
@@ -29,12 +25,10 @@ const Post = props => {
                     onClick={() => setEnableParticles(!enableParticles)}
                 />
                 <SectionWrapper id="blog-homepage" offset={0} minHeight="90vh">
-                    <PostWrapper object={post.body} />
-                    {props.title}
+                    <PostWrapper object={props.body} />
                 </SectionWrapper>
             </Layout>
-            <Footer /> */}
-            {props.id}
+            <Footer />
         </div>
     );
 };
@@ -49,9 +43,12 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params }) {
+    const postsDirectory = path.join(process.cwd(), 'static-data', 'posts.json');
+    const posts = JSON.parse(await fs.readFile(postsDirectory, 'utf8'));
+    const post = posts.find(post => post.id === params.post);
     return {
         props: {
-            id: params.post
+            body: post.body
         },
     };
 }
